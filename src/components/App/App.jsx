ec61e0660b0eaf733b66f7960e0900967941433b
Avery Yang <averyyang2674@gmail.com>
2022-07-14 13:25:40 -0500

    complete customer info

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
+//import CustomerInfo
+import CustomerInfo from '../CustomerInfo/CustomerInfo';
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Prime Pizza</h1>
+            </header>
+
+            <img src="images/pizza_photo.png" />
+            <p>Pizza is great.</p>
+            <CustomerInfo />
+        </div>
+    );
 }
 
 export default App;
