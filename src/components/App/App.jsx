07b5961c88dbedd52ab35a13c0818cca9bd221c5
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-14 11:43:35 -0500

    add styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import CreateOrder from '../CreateOrder/CreateOrder';
 
 function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1 className='App-title'>Prime Pizza</h1>
+			</header>
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
-    </div>
-  );
+			<CreateOrder />
+		</div>
+	);
 }
 
 export default App;
