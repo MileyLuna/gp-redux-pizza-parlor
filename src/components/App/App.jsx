5419834d80ca750b841f057cef6153d6c36ec762
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-14 13:10:43 -0500

    add links

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import axios from 'axios';
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Admin from '../Admin/Admin';
 import './App.css';
 import CreateOrder from '../CreateOrder/CreateOrder';
 
-
 import Checkout from '../Checkout/Checkout';
 
-
 function App() {
-  return (
-    <Router >
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-        <ul>
-        <li>
-          {/* <Link to='/Admin'>Admin</Link> */}
-        </li>
-      </ul>
-      </header>
-      <Route path="/" exact>
-        <CreateOrder />
-      </Route>
-      <Route path="/checkout" exact>
-        <Checkout />
-      </Route>
-      <Route path="/admin" exact>
-        <Admin />
-      </Route>
-    </div>
-    </Router>
-
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					<h1 className='App-title'>Prime Pizza</h1>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/admin'>Admin</Link>
+						</li>
+						<li>
+							<Link to='/checkout'>Admin</Link>
+						</li>
+					</ul>
+				</header>
+				<Route path='/' exact>
+					<CreateOrder />
+				</Route>
+				<Route path='/checkout' exact>
+					<Checkout />
+				</Route>
+				<Route path='/admin' exact>
+					<Admin />
+				</Route>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
